258a1b0e0181b40115ed4c794472eef1b9079a2c
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2024-02-09 20:07:25 +0700

    Update randombg.js

--- files/scripts/randombg.js
+++ files/scripts/randombg.js
@@ -1,4 +1,2 @@
-var images = ['bg1.jpg',
-          'bg2.jpg', 
-          'bg3.jpg'];
-$('#bg').css({'background-image' : 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+var images = ["bg1.jpg","bg2.jpg","bg3.jpg"];
+$('body').css({'background-image' : 'url(https://enderminecraft.github.io/files/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
