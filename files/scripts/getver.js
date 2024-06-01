5875e596b2c456016f610b93a34bede1b4b0bcd9
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2024-06-01 23:25:53 +0700

    Update getver.js

--- files/scripts/getver.js
+++ files/scripts/getver.js
@@ -6,3 +6,11 @@
   }; 
   xhr.open("GET", "https://enderminecraft.github.io/ver.txt", true); 
   xhr.send(); 
+ var xhr1 = new XMLHttpRequest(); 
+  xhr1.onreadystatechange = function() { 
+    if (xhr1.readyState == 4 && xhr1.status == 200) { 
+      document.getElementById("data1").innerHTML = "("+ xhr1.responseText+")"; 
+    } 
+  }; 
+  xhr1.open("GET", "https://enderminecraft.github.io/vercrckdnbr.txt", true); 
+  xhr1.send(); 
