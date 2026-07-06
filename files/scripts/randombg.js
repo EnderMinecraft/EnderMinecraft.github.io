7ca63c408c8b63f7a9c42587897418a438c45b48
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2026-07-06 16:27:53 +0700

    Add files via upload

--- files/scripts/randombg.js
+++ files/scripts/randombg.js
@@ -1,2 +1,14 @@
-var images = ["bg1.jpg","bg2.jpg","bg3.jpg"];
-$('body').css({'background-image' : 'url(https://enderminecraft.github.io/files/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+// Neo-brutalist palette doesn't use photo backgrounds, so instead of swapping
+// in a random image, we randomize which accent color the hero's grid dots
+// and headline pop-shadow use on each visit.
+var accents = ["#ff80c1", "#ffd992", "#94ff99", "#a7c3ff"];
+var pick = accents[Math.floor(Math.random() * accents.length)];
+var backgrndcl = ["#a4b1ff", "#8effe3", "#559e1a", "#ffeab2"];
+var bgPick = backgrndcl[Math.floor(Math.random() * backgrndcl.length)];
+document.documentElement.style.setProperty('--pink', pick);
+document.documentElement.style.setProperty('--bgcl', bgPick);
+
+$(function () {
+    $('.hero h1').css('text-shadow', '4px 4px 0 ' + pick);
+    $('.hero .kicker').css('background', '#0B0B0B');
+});
